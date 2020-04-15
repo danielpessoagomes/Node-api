ad00e7b448bbb663a3419f9ec5addd9a1c7ce2e4
Daniel Gomes <danielgomes@dginfope.com.br>
2020-04-14 23:47:18 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+//Iniciando App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+{ useUnifiedTopology: true, useNewUrlParser: true }
+);
+
+requireDir('./src/models');
+
+//Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
